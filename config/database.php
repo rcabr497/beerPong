c7d1ac093e157378b23b8080c932eb2c75c16577
rcabr497 <info@rickycabrera.com>
2015-12-02 23:11:52 -0500

    got the DB connected

--- config/database.php
+++ config/database.php
@@ -57,7 +57,6 @@ return [
             'host'      => env('DB_HOST', 'localhost'),
             'database'  => env('DB_DATABASE', 'forge'),
             'username'  => env('DB_USERNAME', 'forge'),
-            'unix_socket' => '/path/to/mysql/mysql.sock',
             'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
